4fae0686987542df4e70d1d4ca90e0135e0bbcb2
madebycm <christian.meinhold@gmail.com>
2018-02-21 14:19:36 +0100

    React first test

--- web/src/App.js
+++ web/src/App.js
@@ -3,16 +3,21 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+    getLocation = () => {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(this.showPosition);
+        }
+    }
+    showPosition = (position) => {
+        console.log("hey", position);
+    }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <button onClick={this.getLocation}>Get my location</button>
+        <div id="x"></div>
       </div>
     );
   }
