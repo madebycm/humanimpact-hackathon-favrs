5e75eb503d1dd31dd084c14830a6f8790ec4fb68
Daniel Siwek <danielsiwek95@gmail.com>
2018-02-21 21:33:57 +0100

    Change structure of the main component

--- web/src/App.js
+++ web/src/App.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom'
-import { Provider } from 'react-redux'
-import { ConnectedRouter } from 'react-router-redux'
 
 import Auth from './pages/Auth'
 import Home from './pages/Home'
@@ -10,34 +8,27 @@ import Proximity from './pages/Proximity'
 import Chat from './pages/Chat'
 import api from './utils/api'
 
-import store, { history } from './store'
-
 import './App.css';
 
 class App extends Component {
-
   componentDidMount() {
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(({coords}) => api.geolocation.saveMyPosition({position: coords}));
+      navigator.geolocation.getCurrentPosition(({coords}) => api.geolocation.saveMyPosition({position: coords}));
     }
     else alert("Your device doesnt support this app because of geolocation");
   }
 
   render() {
     return (
-      <Provider store={store}>
-        <ConnectedRouter history={history}>
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/profile" component={Profile} />
-            <Route path="/proximity" component={Proximity} />
-            <Route path="/chat" component={Chat} />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/profile" component={Profile} />
+        <Route path="/proximity" component={Proximity} />
+        <Route path="/chat" component={Chat} />
 
-            <Route path="/auth" component={Auth} />
-            {/* <Route component={NoMatch}/> */}
-          </Switch>
-        </ConnectedRouter>
-      </Provider>
+        <Route path="/auth" component={Auth} />
+        {/* <Route component={NoMatch}/> */}
+      </Switch>
     );
   }
 }
