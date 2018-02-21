95624e029a6015d921639d677ae37bd09b4c95c1
Daniel Siwek <danielsiwek95@gmail.com>
2018-02-21 20:41:31 +0100

    Basic html of Profile page

---
+++ web/src/pages/Profile/index.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+import './style.css'
+
+const Profile = ({}) => (
+  <div className='Profile-page'>
+    <div>
+      Alek Wikli
+    </div>
+
+    <section className='u-mt-'>
+      <div>
+        <div>27</div>
+        <div>High Fives</div>
+      </div>
+
+      <div>
+        Tap - and find someone to take a High-five with
+      </div>
+    </section>
+  </div>
+);
+
+export default Profile;
