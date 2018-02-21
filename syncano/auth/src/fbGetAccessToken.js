06cb7f447646ec8048739d7a0f3c50e8c2c19f91
madebycm <christian.meinhold@gmail.com>
2018-02-21 20:34:44 +0100

    Add backend redirectUri

--- syncano/auth/src/fbGetAccessToken.js
+++ syncano/auth/src/fbGetAccessToken.js
@@ -6,7 +6,9 @@ export default (ctx) => {
   const {debug} = logger('hello script')
 
   const code = ctx.args.code
-  const getAccessTokenUrl = 'https://graph.facebook.com/oauth/access_token?client_id=1747104338635842&redirect_uri=https://api.syncano.io/v2/instances/purple-cherry-5235/endpoints/sockets/auth/fbgetaccesstoken/&client_secret=eb73ff8ffa009d8881bc96894477e347&code=';
+  
+  const redirectUri = ctx.meta.request.HTTP_REFERER + "auth";
+  const getAccessTokenUrl = 'https://graph.facebook.com/oauth/access_token?client_id=1747104338635842&redirect_uri='+redirectUri+'fbgetaccesstoken/&client_secret=eb73ff8ffa009d8881bc96894477e347&code=';
 
   axios.get(getAccessTokenUrl+code).then(r => {
   	debug("got code!", r.data.access_token);
@@ -15,6 +17,6 @@ export default (ctx) => {
   	})
   	.catch(e => console.log(e));
   	//return r.data.access_token;
-  }).catch(e => console.log(e));
+  }).catch(e => console.log("axios fu", e));
 
 }
