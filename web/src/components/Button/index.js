543339840459b97305414b33813b9cadb9db01eb
Paweł Jabłoński <paweljablonski2103@gmail.com>
2018-02-21 19:15:45 +0100

    Fix index.js in Button component

--- web/src/components/Button/index.js
+++ web/src/components/Button/index.js
@@ -1,3 +1,10 @@
-import Button from './Button'
+import React from 'react';
+import './style.css';
+
+const Button = ({ backgroundBlue, onClick, children }) => (
+  <button className={backgroundBlue ? "Button BackgroundBlue" : "Button"} onClick={onClick}>
+    {children}
+  </button>
+);
 
 export default Button
