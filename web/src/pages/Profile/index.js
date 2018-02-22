1efc8562e4ebe3e812e2c45a685669d428523ac3
Daniel Siwek <danielsiwek95@gmail.com>
2018-02-22 02:43:50 +0100

    [Profile page] Redirect to the root url (Login component) if not logged in user goes to profile

--- web/src/pages/Profile/index.js
+++ web/src/pages/Profile/index.js
@@ -1,62 +1,78 @@
-import React from 'react'
+import React, {PureComponent} from 'react'
 import {connect} from 'react-redux'
+import { withRouter } from 'react-router-dom'
 
 import {Header, Wrapper} from '../../components'
 
 import './style.css'
 
-const Profile = ({user}) => (
-  <div className='Page'>
-    <Header />
+class Profile extends PureComponent {
+  componentWillMount() {
+    const {user, history} = this.props
+    const token = window.localStorage.getItem('token')
 
-    <Wrapper>
-      <section className='High-fives u-mt+ u-flex-row u-center'>
-        <div className='High-fives__item u-mr-- u-ta-c u-width-half '>
-          <div className='High-fives__item__counter'>27</div>
-          <div>High Fives</div>
-        </div>
+    if (!token) {
+      history.push('/')
+    }
+  }
 
-        <div className='High-fives__item u-ml-- u-ta-c u-width-half '>
-          Tap - and find someone to take a High-five with
-        </div>
-      </section>
-    </Wrapper>
+  render() {
+    const {user} = this.props
 
-    <section className='High-fives-lists u-mt'>
-      <header className='u-space-between'>
-        <span className='u-width-half u-mili u-mr- u-center'>MY HIGH-FIVES</span>
-        <span className='u-width-half u-mili u-ml- u-center'>COMMUNITY HIGH-FIVES</span>
-      </header>
+    return (
+      <div className='Page'>
+        <Header />
 
-      <Wrapper>
-        <div className='u-space-between u-mt--'>
-          <ul className='High-fives-list u-width-half u-mr--'>
-            <li className='High-fives-list__item'>
-              <aside className='badge'>HIGH FIVE</aside>
-              <img src="http://lorempixel.com/g/100/300" alt=""/>
-            </li>
-            <li className='High-fives-list__item'>
-              <aside className='badge'>HIGH FIVE</aside>
-              <img src="http://lorempixel.com/g/100/300" alt=""/>
-            </li>
-          </ul>
+        <Wrapper>
+          <section className='High-fives u-mt+ u-flex-row u-center'>
+            <div className='High-fives__item u-mr-- u-ta-c u-width-half '>
+              <div className='High-fives__item__counter'>27</div>
+              <div>High Fives</div>
+            </div>
 
-          <ul className='High-fives-list u-width-half u-ml--'>
-            <li className='High-fives-list__item'>
-              <aside className='badge'>HIGH FIVE</aside>
-              <img src="http://lorempixel.com/g/100/200" alt=""/>
-            </li>
-            <li className='High-fives-list__item'>
-              <aside className='badge'>HIGH FIVE</aside>
-              <img src="http://lorempixel.com/g/100/200" alt=""/>
-            </li>
-          </ul>
-        </div>
-      </Wrapper>
-    </section>
-  </div>
-);
+            <div className='High-fives__item u-ml-- u-ta-c u-width-half '>
+              Tap - and find someone to take a High-five with
+            </div>
+          </section>
+        </Wrapper>
 
-export default connect(({user}) => ({
+        <section className='High-fives-lists u-mt'>
+          <header className='u-space-between'>
+            <span className='u-width-half u-mili u-mr- u-center'>MY HIGH-FIVES</span>
+            <span className='u-width-half u-mili u-ml- u-center'>COMMUNITY HIGH-FIVES</span>
+          </header>
+
+          <Wrapper>
+            <div className='u-space-between u-mt--'>
+              <ul className='High-fives-list u-width-half u-mr--'>
+                <li className='High-fives-list__item'>
+                  <aside className='badge'>HIGH FIVE</aside>
+                  <img src="http://lorempixel.com/g/100/300" alt=""/>
+                </li>
+                <li className='High-fives-list__item'>
+                  <aside className='badge'>HIGH FIVE</aside>
+                  <img src="http://lorempixel.com/g/100/300" alt=""/>
+                </li>
+              </ul>
+
+              <ul className='High-fives-list u-width-half u-ml--'>
+                <li className='High-fives-list__item'>
+                  <aside className='badge'>HIGH FIVE</aside>
+                  <img src="http://lorempixel.com/g/100/200" alt=""/>
+                </li>
+                <li className='High-fives-list__item'>
+                  <aside className='badge'>HIGH FIVE</aside>
+                  <img src="http://lorempixel.com/g/100/200" alt=""/>
+                </li>
+              </ul>
+            </div>
+          </Wrapper>
+        </section>
+      </div>
+    )
+  }
+}
+
+export default withRouter(connect(({user}) => ({
   user: user.profile
-}))(Profile)
+}))(Profile))
