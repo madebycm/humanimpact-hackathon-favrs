3fd555b3438cae12752eea0c7d633c61c3d6471b
Daniel Siwek <danielsiwek95@gmail.com>
2018-02-22 03:01:05 +0100

    [Proximity page] Render Header

--- web/src/pages/Proximity/index.js
+++ web/src/pages/Proximity/index.js
@@ -1,11 +1,25 @@
 import React from 'react';
-import Table from '../../components/Table'
+import { withRouter } from 'react-router-dom'
+import {Header, Table, IconButton} from '../../components'
 
 import './style.css'
 
-const Proximity = () => (
+const Proximity = ({history}) => (
   <div className='Page'>
+    <Header>
+      <IconButton
+        text='Back to profile'
+        onClick={() => history.push('/profile')}
+      />
+    </Header>
+
+    <p className='u-ta-c'>
+      Find someone close. <br/>
+      Tap on profile to chat and arrange your High Five
+    </p>
+
     <Table
+      className='u-mt'
       rows={[
         {
           name: 'Thomas',
@@ -20,4 +34,4 @@ const Proximity = () => (
   </div>
 );
 
-export default Proximity;
+export default withRouter(Proximity);
