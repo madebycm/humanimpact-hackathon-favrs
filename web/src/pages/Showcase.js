46f7ac3b4ec5840be6ea43541a64b2fd952452c5
Paweł Jabłoński <paweljablonski2103@gmail.com>
2018-02-22 03:53:58 +0100

    Create CircleButton component

---
+++ web/src/pages/Showcase.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import Button from '../components/Button'
+import Table from '../components/Table'
+import IconButton from '../components/IconButton'
+import CircleButton from '../components/CircleButton'
+// import Avatar from '../components/Avatar'
+
+const membersList = [
+  { name: "John", url: "", distance: "0,3km" },
+  { name: "Mark", url: "", distance: "0,8km" },
+  { name: "John", url: "", distance: "1,6km" }
+]
+
+const Showcase = ({}) => (
+  <div>
+    <h1>Showcase page</h1>
+    <h2>Button:</h2>
+    <div>
+      <Button>
+        Sign in with facebook
+      </Button>
+      <Button backgroundBlue>
+        Sign in with facebook
+      </Button>
+    </div>
+    <h2>Icon Button:</h2>
+    <div>
+      <IconButton text="Back to list" />
+      <IconButton text="Back to list" />
+    </div>
+    <h2>Circle Button</h2>
+    <CircleButton
+      fileName="HighFiveIcon.png"
+      text="Confirm"
+    />
+    <h2>List:</h2>
+    <Table
+      rows={membersList}
+    />
+    <h2>Avatar:</h2>
+    {/* <Avatar /> */}
+  </div>
+);
+
+export default Showcase;
