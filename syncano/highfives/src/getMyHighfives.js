4fb9ef469ca638fbaceb0ef11e5e2f61f2237cc6
madebycm <christian.meinhold@gmail.com>
2018-02-22 14:00:52 +0100

    Get hfs in profile socket

--- syncano/highfives/src/getMyHighfives.js
+++ syncano/highfives/src/getMyHighfives.js
@@ -3,11 +3,9 @@ import Syncano from 'syncano-server'
 export default async (ctx) => {
 
   const {users, response, data} = Syncano(ctx)
-
-  if(!ctx.meta.user) response.json({err: "No user in meta"})
-
+  
   const allHighFives = await data.highfives.list()
-  const myHighFives = allHighFives.find(hf => hf.to_user === ctx.meta.user.id || hf.from_user === ctx.meta.user.id)
+  const myHighFives = allHighFives.find(hf => hf.to_user === ctx.args.user_id || hf.from_user === ctx.args.user_id)
   
   response.json(myHighFives)
 
--- syncano/users/src/getMyProfile.js
+++ syncano/users/src/getMyProfile.js
@@ -1,14 +1,17 @@
 import Syncano from 'syncano-server'
 
-export default (ctx) => {
-  const {users, response} = Syncano(ctx)
+export default async (ctx) => {
+  const {users, response, socket} = Syncano(ctx)
 
   console.log("Hello, Siwek", ctx.meta.user);
-  
+
   if(!ctx.meta.user) return response.json({error: "No user found in context magic syncano meta stuff", user: false}, 400);
 
+  const allhf = await socket.get("/highfives/getAllHighfives")
+  const myhf = await socket.get("/highfives/getMyHighfives", {user_id: ctx.meta.user.id})
+
   users.find(ctx.meta.user.id).then(e => {
-    return response.json({user: e})
+    return response.json({user: e, myhf: myhf, allhf: allhf})
   }).catch(e => console.log(e))
 
 }
