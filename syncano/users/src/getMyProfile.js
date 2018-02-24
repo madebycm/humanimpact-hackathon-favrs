a9b486ce714d1bb93c330d3e681f5111f78404b4
Daniel Siwek <danielsiwek95@gmail.com>
2018-02-24 14:29:24 +0100

    [Syncano][users] Don't attach highfives to the profile object

--- syncano/users/src/getMyProfile.js
+++ syncano/users/src/getMyProfile.js
@@ -3,15 +3,13 @@ import Syncano from 'syncano-server'
 export default async (ctx) => {
   const {users, response, socket} = Syncano(ctx)
 
-  console.log("Hello, Siwek", ctx.meta.user);
+  if(!ctx.meta.user) return response.json({error: "No user found in context magic syncano meta stuff", user: {}}, 400);
 
-  if(!ctx.meta.user) return response.json({error: "No user found in context magic syncano meta stuff", user: false}, 400);
+  // const allhf = await socket.get("/highfives/getAllHighfives", {user_id: ctx.meta.user.id})
+  // const myhf = await socket.get("/highfives/getMyHighfives", {user_id: ctx.meta.user.id})
 
-  const allhf = await socket.get("/highfives/getAllHighfives", {user_id: ctx.meta.user.id})
-  const myhf = await socket.get("/highfives/getMyHighfives", {user_id: ctx.meta.user.id})
-
-  users.find(ctx.meta.user.id).then(e => {
-    return response.json({user: e, myhf: myhf, allhf: allhf})
-  }).catch(e => console.log(e))
+  users.find(ctx.meta.user.id)
+    .then(user => response.json(user))
+    .catch(e => console.log(e))
 
 }
