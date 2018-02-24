8b5910880162c2f7499f3fccbe963f1953d322e9
Daniel Siwek <danielsiwek95@gmail.com>
2018-02-24 14:29:44 +0100

    [Syncano][highfives] Set confirmed status on confirm endpoint

--- syncano/highfives/src/confirm.js
+++ syncano/highfives/src/confirm.js
@@ -4,22 +4,17 @@ import FormData from 'form-data'
 export default (ctx) => {
   const {data, response} = Syncano(ctx)
 
-  // if(!ctx.meta.user){
-  // 	response.json({error: "No user found"}, 403);
-  // 	return;
-  // }
-
   if (ctx.args.photo) {
     const form = new FormData()
     form.append('photo', ctx.args.photo, {
       filename: ctx.args.filename,
       filetype: ctx.args.filetype
     })
+    form.append('status', 'confirmed')
 
     data.highfives.update(ctx.args.id, form)
     .then(r => {
-      // upload here
-      response.json({status: "OK", response: r});
+      response.json(r);
     })
     .catch(e => console.log("Fucked up", e))
   }
