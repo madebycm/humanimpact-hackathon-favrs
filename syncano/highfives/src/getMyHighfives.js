8b7dfdaaee9d476ea5653de1001084253c44474d
Daniel Siwek <danielsiwek95@gmail.com>
2018-02-24 14:28:53 +0100

    [Syncano][highfives] Fix getMyHighFives endpoint

--- syncano/highfives/src/getMyHighfives.js
+++ syncano/highfives/src/getMyHighfives.js
@@ -3,12 +3,18 @@ import Syncano from 'syncano-server'
 export default async (ctx) => {
 
   const {users, response, data} = Syncano(ctx)
-  
-  let allHighFives = await data.highfives.list()
-  // allHighFives = allHighFives.filter(hf => hf.to_user != ctx.args.user_id || hf.from_user != ctx.args.user_id)
 
-  const myHighFives = allHighFives.filter(hf => hf.to_user === ctx.args.user_id || hf.from_user === ctx.args.user_id)
-  
-  response.json(myHighFives)
+  if(!ctx.meta.user) return response.json({error: "No user found in context magic syncano meta stuff", user: {}}, 400);
 
+  try {
+    const allHighFives = await data.highfives.list()
+
+    const myHighFives = allHighFives
+    .filter(hf => hf.status === 'confirmed')
+    .filter(hf => hf.to_user === ctx.meta.user.id || hf.from_user === ctx.meta.user.id)
+
+    response.json(myHighFives)
+  } catch (e) {
+    response.json(e, 400)
+  }
 }
